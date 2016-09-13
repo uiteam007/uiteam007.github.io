6379d2d1f0314a9e876d1364b3c174a31d1c287c
Rajkanwal williamjit Singh <Rajkanwal williamjit Singh>
2016-09-12 19:04:06 -0700

    form

--- index.js
+++ index.js
@@ -1,13 +1,8 @@
 var myapp = angular.module("angjspapp", []);
-myapp.controller("angjspctrl", function ($scope, $http) {
+myapp.controller("angjspctrl", function ($scope) {
 
-  $scope.PressForMovie = function () {
-    var URL = "http://api.rottentomatoes.com/api/public/v1.0/movies.json?apikey=bzm7bkpkkm4wghg22xdbx4vy&q=" + $scope.input + "&callback=JSON_CALLBACK";
-
-    $scope.mymovies = [];
-    $http.jsonp(URL).success(function (resp) {
-      $scope.mymovies = resp.movies;
-      console.log($scope.mymovies);
-    });
-  };
+ $scope.start="form"
+  
 });
+
+
